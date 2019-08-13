e0c3c15f5a218b2accca4c46d87dca64d2fc695c
tearth <tearthdev@gmail.com>
2019-08-13 15:56:50 +0200

    Add generator of air voxels.

--- RealShipPhysics/Assets/Scripts/AirArea.cs
+++ RealShipPhysics/Assets/Scripts/AirArea.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-[ExecuteInEditMode]
 public class AirArea : MonoBehaviour
 {
-    public Vector3 Size;
+    public Vector3Int Size;
+    public AirVoxel AirVoxelPrefab;
+
+    private AirVoxel[,,] _airGrid;
+
+    void Awake()
+    {
+        ClearAirGrid();
+        GenerateAirGrid();
+    }
 
     void Start()
     {
@@ -17,7 +26,8 @@ public class AirArea : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        var centerOfArea = (Size / 2) - new Vector3(0.5f, 0.5f, 0.5f);
+        var centerOfArea = ((Vector3)Size / 2) - new Vector3(0.5f, 0.5f, 0.5f);
+        var startPoint = transform.position - centerOfArea;
 
         for (var x = 0; x < Size.x; x++)
         {
@@ -25,9 +35,41 @@ public class AirArea : MonoBehaviour
             {
                 for (var z = 0; z < Size.z; z++)
                 {
-                    Gizmos.DrawWireCube(transform.position - centerOfArea + new Vector3(x, y, z), Vector3.one);
+                    Gizmos.DrawWireCube(startPoint + new Vector3(x, y, z), Vector3.one);
                 }
             }
         }
+    }
+
+    private void ClearAirGrid()
+    {
+        var destroyedAirVoxels = 0;
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+            destroyedAirVoxels++;
+        }
+
+        Debug.Log($"{destroyedAirVoxels} air voxels destroyed");
+    }
+
+    private void GenerateAirGrid()
+    {
+        _airGrid = new AirVoxel[Size.x, Size.y, Size.z];
+
+        var id = 0;
+        for (var x = 0; x < Size.x; x++)
+        {
+            for (var y = 0; y < Size.y; y++)
+            {
+                for (var z = 0; z < Size.z; z++)
+                {
+                    _airGrid[x, y, z] = Instantiate(AirVoxelPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
+                    _airGrid[x, y, z].name = $"AirVoxel {id++}";
+                }
+            }
+        }
+
+        Debug.Log($"{_airGrid.Length} air voxels generated");
     }
 }
